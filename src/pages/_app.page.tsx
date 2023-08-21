35d7425321c4ccd5db41c98522f1d4494b44b487
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-08-20 21:05:19 -0300

    feat: add layout

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,26 @@
 import { globalStyles } from '@/styles/global'
+import { NextPage } from 'next'
 import { DefaultSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 import { Nunito } from 'next/font/google'
+import { ReactElement, ReactNode } from 'react'
 
 globalStyles()
 
 const nunito = Nunito({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
-export default function App({ Component, pageProps }: AppProps) {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
   return (
     <>
       <DefaultSeo
@@ -20,7 +33,7 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       />
       <main className={nunito.className}>
-        <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
       </main>
     </>
   )
